cb6739b372d920d11107e86c55bff0f1d8db4d6f
iincp <89747457+iincp@users.noreply.github.com>
2023-06-04 22:37:40 +0700

    Update room.py

--- room.py
+++ room.py
@@ -3,69 +3,93 @@ from abc import ABC, abstractmethod
 
 class Room(ABC):
     
-    def __init__(self, room_id, room_type, max_quantity, booked_room_quantity, days_booked):
+    def __init__(self, room_id, quantity,checkIn,checkOut,status ):
         self.room_id = room_id
-        self.room_type = room_type
-        self.max_quantity = max_quantity
-        self.booked_room_quantity = booked_room_quantity
-        self.days_booked = days_booked
-        self.rooms_available = set([f"{room_id}{i}" for i in range(1, max_quantity + 1)])
+        self.quantity = quantity
+        self.checkInDate =  checkIn
+        self.checkOutDate = checkOut
+        self.payment_status = status 
+        # self.days_booked = days_booked
+        # self.rooms_available = set([f"{room_id}{i}" for i in range(1, max_quantity + 1)])
 
     # randomly books available rooms in the list called rooms_available
-    def book_rooms(self, booked_room_quantity):
-        from account import Account
-        logged_in_user = Account.logged_in_user
-        if len(self.rooms_available) >= booked_room_quantity:
-            # Get the specified number of available rooms
-            booked_rooms = set(list(self.rooms_available)[:booked_room_quantity])
-            self.rooms_available -= booked_rooms
-            booked_room_quantity = booked_room_quantity
-            print(
-                f"{logged_in_user}Successfully booked {booked_room_quantity} {self.room_type} room(s): {booked_rooms}")
-            return booked_rooms  # Return the set of booked room IDs
-        else:
-            print(f"Not enough {self.room_type} rooms available.")
-            return set()  # Return an empty set if booking failed
+    # def book_rooms(self, booked_room_quantity):
+    #     from account import Account
+    #     logged_in_user = Account.logged_in_user
+    #     if len(self.rooms_available) >= booked_room_quantity:
+    #         # Get the specified number of available rooms
+    #         booked_rooms = set(list(self.rooms_available)[:booked_room_quantity])
+    #         self.rooms_available -= booked_rooms
+    #         booked_room_quantity = booked_room_quantity
+    #         print(
+    #             f"{logged_in_user}Successfully booked {booked_room_quantity} {self.room_type} room(s): {booked_rooms}")
+    #         return booked_rooms  # Return the set of booked room IDs
+    #     else:
+    #         print(f"Not enough {self.room_type} rooms available.")
+    #         return set()  # Return an empty set if booking failed
 
-    def list_available_rooms(self):
-        print(f"Available {self.room_type} rooms:")
-        for room in self.rooms_available:
-            print(room)
+    # def list_available_rooms(self):
+    #     print(f"Available {self.room_type} rooms:")
+    #     for room in self.rooms_available:
+    #         print(room)
 
     @abstractmethod
     def get_cost(self):
         pass
+    @abstractmethod  
+    def getRoomType(self): 
+        pass
+
+    def getRoomID(self) : 
+        return self.room_id 
+    
+    def getQuantity(self): 
+        return self.quantity 
+    
+    def getCheckInDate(self) : 
+        return self.CheckInDate 
+    def getCheckOutDate(self): 
+        return self.CheckOutdate
  
 class MixedDorm(Room):
     #innitialize days bookeds
-    def __init__(self):
+    def __init__(self, room_id, quantity,checkIn,checkOut ):
         #(room_id, room_type, max_quantity)
-        super().__init__('M', 'Mixed Dorm', 10, 0, 0)
+        super().__init__(room_id, quantity,checkIn,checkOut )
 
-    def get_cost(self, booked_room_quantity, days_booked):
-        cost = (800 * booked_room_quantity) * days_booked
+    def get_cost(self, quantity, days_booked):
+        cost = (800 * quantity) * days_booked
         return cost
+    def getRoomType(self): 
+        return "MIXED DORM"
 
 class DeluxeRoom(Room):
-    def __init__(self):
-        super().__init__('D', 'Deluxe', 5, 0, 0)
+    def __init__(self, room_id, quantity,checkIn,checkOut ):
+        #(room_id, room_type, max_quantity)
+        super().__init__(room_id, quantity,checkIn,checkOut )
 
-    def get_cost(self, booked_room_quantity, days_booked):
-        cost = (3500 * booked_room_quantity) * days_booked
+    def get_cost(self, quantity, days_booked):
+        cost = (3500 * quantity) * days_booked
         return cost
+    def getRoomType(self): 
+        return "DELUXE ROOM"
 
 
 class SuperiorRoom(Room):
-    def __init__(self):
-        super().__init__('S', 'Superior', 3, 0, 0)
+    def __init__(self, room_id, quantity,checkIn,checkOut ):
+        #(room_id, room_type, max_quantity)
+        super().__init__(room_id, quantity,checkIn,checkOut )
 
-    def get_cost(self, booked_room_quantity, days_booked):
-        cost = (5000 * booked_room_quantity) * days_booked
+    def get_cost(self, quantity, days_booked):
+        cost = (5000 * quantity) * days_booked
         return cost
     
-def calculate_total_price(Mcost, Dcost, Scost):
-    total_price = Mcost + Dcost + Scost
-    return total_price
+    def getRoomType(self): 
+        return "SUPERIOR ROOM"
+    
+# def calculate_total_price(Mcost, Dcost, Scost):
+#     total_price = Mcost + Dcost + Scost
+#     return total_price
     
 
 # mixed_dorm = MixedDorm()
